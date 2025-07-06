4dcd1c1751897ccd5410cc371e90a39e87f1c8e6
philles85 <philster04@outlook.com>
2025-07-06 16:55:34 +0200

    Updated the server
    I have updated and added the server endpoints to work with the api and client

--- API/server.js
+++ API/server.js
@@ -1,13 +1,16 @@
 const allUsersRoute = new URLPattern({ pathname: "/allUsers" });
+const addFriendsRoute = new URLPattern({ pathname: "/addToFriendsList" });
+const getFriendsRoute = new URLPattern({ pathname: "/getFriendList" });
+const removeFriendsRoute = new URLPattern({ pathname: "/removeFriend" });
 
 function readData() {
     const dataPath = "../data/users.json";
     return JSON.parse(Deno.readTextFile(dataPath));
 }
 
-function writeData(data) {
+async function writeData(data) {
     const dataPath = "../data/users.json";
-    Deno.writeTextFile(dataPath, JSON.stringify(data));
+    await Deno.writeTextFile(dataPath, JSON.stringify(data, null, 2));
 }
 
 
@@ -16,6 +19,9 @@ async function handler(request) {
     const url = new URL(request.url);
 
     const allUsersMatch = allUsersRoute.exec(url);
+    const addFriendsMatch = addFriendsRoute.exec(url);
+    const getFriendsMatch = getFriendsRoute.exec(url);
+    const removeFriendsMatch = removeFriendsRoute.exec(url);
 
 
     const headerCORS = new Headers();
@@ -34,7 +40,46 @@ async function handler(request) {
     }
 
     if (allUsersMatch) {
-        return new Response(JSON.stringify(await userInfo()), { headers: headerCORS, status: 200 })
+        if (request.method == "GET") {
+            return new Response(JSON.stringify(await userInfo()), { headers: headerCORS, status: 200 })
+        }
+    }
+
+    if (addFriendsMatch) {
+        if (request.method == "POST") {
+            const data = await request.json();
+            const allUsers = await userInfo();
+            const correctUser = allUsers.users.find(dataId => dataId.id == data.userId);
+            const fileData = await readData();
+            const parsedData = JSON.parse(fileData);
+            if (correctObjekt) {
+                parsedData.push(correctUser)
+                writeData(JSON.stringify(parsedData));
+                return new Response("Success!", { headers: headerCORS, status: 200 })
+            } else {
+                return new Response("Unsucces!", { headers: headerCORS, status: 409 })
+            }
+        }
+    }
+
+    if (getFriendsMatch) {
+        if (request.method == "GET") {
+            const allUserData = await readData();
+            const parsedData = JSON.parse(allUserData);
+            return new Response(JSON.stringify(parsedData), { headers: headerCORS, status: 200 })
+        }
+    }
+
+    if (removeFriendsMatch) {
+        if (request.method == "DELETE") {
+            const userID = await request.json();
+            const allUserData = await readData();
+            const allUserDataParsed = JSON.parse(allUserData);
+            const friendUser = allUserDataParsed.find(user => user.id == userID.friendId);
+            const newUsers = allUserDataParsed.filter(users => users.id != userID.friendId);
+            writeData(JSON.stringify(newUsers));
+            return new Response(JSON.stringify(friendUser), { headers: headerCORS, status: 200 });
+        }
     }
 }
 
